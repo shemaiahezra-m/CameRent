cac68a660353abec6bdf3e26a3a1f3a8a3d33142
Shemaiah Ezra <magpayo.shemaiahezra@gmail.com>
2026-05-19 14:02:21 +0800

    Removed unused folders

--- Oct 10/scrpt.js
+++
@@ -1,8 +0,0 @@
- document.getElementsByTagName("p")[0].onmouseover = function() {
-        this.innerText = 'Pink is her fav color' ;this.style.color = 'red';
-    }
-
- document.getElementsByTagName("p")[0].onmouseout = function() {
-        this.innerText = 'More me'; this.style.color = 'black';
-    }
-    
